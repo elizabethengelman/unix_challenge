1089116c7df0df15f6c17a2920e492e686cd3ce0
Apprentice <apprentice@Alexs-Mac-mini.local>
2013-11-27 15:08:45 -0600

    method now reads files only 100 bytes at a time

--- cat.rb
+++ cat.rb
@@ -2,10 +2,10 @@ def cat(files)
   files.each do |file|
     if File.exist?(file)
       input_file = File.open(file, 'r')
-      opened_file = input_file.read
-      puts opened_file
+      until input_file.eof
+        puts input_file.read(100)
+      end
     else
-      # $stderr.write("This file doesn't exist")
        abort "This file doesn't exist"
     end
   end
