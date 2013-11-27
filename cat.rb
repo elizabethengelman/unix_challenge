9e1998f432a0e7c3eab685e579a3b57cb2c1c31d
Apprentice <apprentice@Alexs-Mac-mini.local>
2013-11-27 14:08:09 -0600

    are able to read and print multiple files

--- cat.rb
+++ cat.rb
@@ -1,3 +1,9 @@
-file = File.open(ARGV[0],'r')
-opened_file = file.read
-puts opened_file
+def cat(files)
+  files.each do |file|
+    input_file = File.open(file, 'r')
+    opened_file = input_file.read
+    puts opened_file
+  end
+end
+
+cat(ARGV)
